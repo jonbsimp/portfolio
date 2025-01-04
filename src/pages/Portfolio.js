26a9656a23b893ecad8163ae6001c41e9c666314
jonbsimp <jonbsimp@gmail.com>
2025-01-03 21:43:03 -0700

    refresh

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -9,12 +9,7 @@ export default function Portfolio() {
             <p>Here are some of the projects I've worked on:</p>
             <ul>
                 <li>
-                    <p>Forecasting Application for a Fortune 500 Company</p>
-                    <p>Improved headcount predictions for a healthcare and retail giant.</p>
-                    <p>Flagship Applications for Major Sports Teams</p>
-                    <p>Streamlined operational workflows and enhanced user experience.</p>
-                    <p>Legacy Database Migration</p>
-                    <p>Optimized data accessibility and reporting for a retail giant.</p>
+      
                 </li>
             </ul>
         </div>
